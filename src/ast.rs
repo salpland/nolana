bc3ea84660106a8bf51dfdf582d9703d1aadb533
arexon <arexonreal@gmail.com>
2023-02-05 23:33:40 +0200

    feat: initial AST

---
+++ src/ast.rs
@@ -0,0 +1,24 @@
+pub enum Statement {
+    Expr(Expression),
+}
+
+pub enum Expression {
+    Binary(Box<Expression>, Operator, Box<Expression>),
+    Unary(Operator, Box<Expression>),
+    Number(f64),
+}
+
+impl Expression {
+    /// Helper to wrap the expression into a box smart pointer.
+    pub fn boxed(self) -> Box<Self> {
+        Box::new(self)
+    }
+}
+
+pub enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Neg,
+}
