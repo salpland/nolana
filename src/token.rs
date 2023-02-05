d6fe669340ef2ab42e3af8e6ab0858208c67d665
arexon <arexonreal@gmail.com>
2023-02-05 23:57:49 +0200

    upd(token): add unit tests

--- src/token.rs
+++ src/token.rs
@@ -1,12 +1,12 @@
 use logos::Logos;
 
-#[derive(Logos)]
+#[derive(Debug, PartialEq, Logos)]
 pub enum Token<'a> {
-    #[regex("[a-zA-Z]+.[a-zA-Z_]+")]
+    #[regex("[a-zA-Z]+[.][a-zA-Z_]+")]
     Identifier(&'a str),
 
     #[regex("'[^']*'")]
-    String(&'a str),
+    Literal(&'a str),
 
     #[regex("([0-9]*[.])?[0-9]+", |lex| lex.slice().parse())]
     Number(f64),
@@ -45,7 +45,7 @@ pub enum Token<'a> {
     GreaterThan,
 
     #[token(">=")]
-    GreaterThanEqualual,
+    GreaterThanEqual,
 
     #[token("<")]
     LessThan,
@@ -116,4 +116,109 @@ pub enum Token<'a> {
     #[regex("[ \t\n\r]+", logos::skip)]
     #[error]
     Error,
+}
+
+#[cfg(test)]
+mod tests {
+    use logos::Logos;
+
+    use super::*;
+
+    macro_rules! text_lexer {
+        ($name:ident, $input:literal, $expected_output:expr) => {
+            #[test]
+            fn $name() {
+                let actual_output: Vec<_> = Token::lexer($input).spanned().collect();
+                assert_eq!(actual_output, $expected_output);
+            }
+        };
+    }
+
+    text_lexer!(comments_and_whitespace, "\t\r# foo bar\n", vec![]);
+
+    text_lexer!(
+        numbers,
+        "1 16 1.5 .25",
+        vec![
+            (Token::Number(1.0), 0..1),
+            (Token::Number(16.0), 2..4),
+            (Token::Number(1.5), 5..8),
+            (Token::Number(0.25), 9..12)
+        ]
+    );
+
+    text_lexer!(
+        literals,
+        "'foo:bar_baz' ''",
+        vec![
+            (Token::Literal("'foo:bar_baz'"), 0..13),
+            (Token::Literal("''"), 14..16),
+        ]
+    );
+
+    text_lexer!(
+        identifiers,
+        "foo.bar_baz Foo.Bar",
+        vec![
+            (Token::Identifier("foo.bar_baz"), 0..11),
+            (Token::Identifier("Foo.Bar"), 12..19)
+        ]
+    );
+
+    text_lexer!(
+        keywords,
+        "return continue break for_each loop true false",
+        vec![
+            (Token::Return, 0..6),
+            (Token::Continue, 7..15),
+            (Token::Break, 16..21),
+            (Token::ForEach, 22..30),
+            (Token::Loop, 31..35),
+            (Token::True, 36..40),
+            (Token::False, 41..46)
+        ]
+    );
+
+    text_lexer!(
+        symbols,
+        "; : , -> ! = == != < > <= >= || && ?? + - * / () {} []",
+        vec![
+            (Token::Semi, 0..1),
+            (Token::Colon, 2..3),
+            (Token::Comma, 4..5),
+            (Token::MinusGreaterThan, 6..8),
+            (Token::Bang, 9..10),
+            (Token::Equal, 11..12),
+            (Token::EqualEqual, 13..15),
+            (Token::BangEqual, 16..18),
+            (Token::LessThan, 19..20),
+            (Token::GreaterThan, 21..22),
+            (Token::LessThanEqual, 23..25),
+            (Token::GreaterThanEqual, 26..28),
+            (Token::BarBar, 29..31),
+            (Token::AndAnd, 32..34),
+            (Token::QuestionQuestion, 35..37),
+            (Token::Plus, 38..39),
+            (Token::Minus, 40..41),
+            (Token::Star, 42..43),
+            (Token::Slash, 44..45),
+            (Token::OpenParen, 46..47),
+            (Token::CloseParen, 47..48),
+            (Token::OpenBrace, 49..50),
+            (Token::CloseBrace, 50..51),
+            (Token::OpenBracket, 52..53),
+            (Token::CloseBracket, 53..54),
+        ]
+    );
+
+    text_lexer!(
+        invalid_input,
+        "1.23.45 foo 'bar",
+        vec![
+            (Token::Number(1.23), 0..4),
+            (Token::Number(0.45), 4..7),
+            (Token::Error, 8..11),
+            (Token::Error, 12..16),
+        ]
+    );
 }
